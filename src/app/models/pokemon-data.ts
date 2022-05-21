572bbef1c2d624fa8ae07bcd3bd5188bbfa4b12c
danialAmiri <danial.amiri.h95@gmail.com>
2022-05-21 14:02:03 +0200

    create the interface models

---
+++ src/app/models/pokemon-data.ts
@@ -0,0 +1,19 @@
+export interface PokemonData {
+  abilities: Array<any>;
+   base_experience: number;
+   forms: Array<any>;
+   game_indices: Array<any>;
+   height: number;
+   held_items: Array<any>;
+   id: number;
+   is_default: boolean;
+   location_area_encounters: string;
+   moves: Array<any>;
+   name: string;
+   past_types: Array<any>;
+   species: any;
+   sprites: any;
+   stats:Array<any>;
+   types: Array<any>;
+   weight: number;
+}
