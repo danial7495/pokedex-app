730dc9cf128d9b72a47969686b389c261130bbbc
danialAmiri <danial.amiri.h95@gmail.com>
2022-05-21 13:59:08 +0200

    send the data to parentcomponent with EventEmitter

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { GetDataService } from '../get-data.service';
+import { PokemonResult } from '../models/pokemon-result';
 
 @Component({
   selector: 'app-input',
@@ -8,18 +9,20 @@ import { GetDataService } from '../get-data.service';
 })
 export class InputComponent implements OnInit {
 
+  @Output() PokemonList = new EventEmitter<any[]>(); // use otuput to send the subscribed data to the parent-component
   allPekomons : any[] = []; //we can use interface and have a model instead any, i try to do that at the end of project, if just time let me
   constructor(private getDataService : GetDataService) { }
 
   ngOnInit(): void {
     this.getDataService.getPokemons()
     .subscribe((response: any) => {
-      response.results.forEach((result:any) => {
+      response.results.forEach((result:PokemonResult) => {
         this.getDataService.getPokemonByName(result.name).subscribe((res) => {
           this.allPekomons.push(res);
         });
       });
     });
+    this.PokemonList.emit(this.allPekomons);
   }
 
 }
