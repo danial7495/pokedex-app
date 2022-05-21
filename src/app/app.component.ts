8aaf6ea74e8e7d2d1b11d00169f4a9c8e24cd555
danialAmiri <danial.amiri.h95@gmail.com>
2022-05-21 14:01:03 +0200

    get the data in appcomponent und put it in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonData } from './models/pokemon-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  allpokemonFromParent: PokemonData[] = [];
   title = 'pokedex-app';
+
+  pokemonParentUpdate($event:any) {
+    this.allpokemonFromParent = $event; // push the recieved data from our first child-component (pokemon-list)
+   }
 }
